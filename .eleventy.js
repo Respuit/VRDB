b5097de8addd4f2fdee2ced096ce94877e9a669c
Respuit <sergy_196@hotmail.com>
2024-12-07 20:49:45 +0100

    failed test

--- .eleventy.js
+++ .eleventy.js
@@ -7,14 +7,22 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/images");
     eleventyConfig.setDataDirectory("../_data");
 
+    // Si es necesario, puedes agregar un filtro para escapar contenido específico
     eleventyConfig.addFilter("escapeAndReplace", (content) => {
-
         const escapedContent = content
             .replace(/&/g, "&amp;") // Escapar `&`
             .replace(/</g, "&lt;") // Escapar `<`
             .replace(/>/g, "&gt;"); // Escapar `>`
 
         return escapedContent.replace(/\n/g, "<br>");
+    });
+
+    // Asegúrate de que se pase como JSON en lugar de HTML
+    eleventyConfig.addExtension("json", {
+        outputFileExtension: "json",
+        compile: function(inputContent) {
+            return inputContent; // Evita la conversión a HTML
+        }
     });
 
     return {
@@ -26,7 +34,6 @@ module.exports = function (eleventyConfig) {
 }
 
 module.exports.config = {
-	pathPrefix: "/home/respuit/Documents/Projects/mockupwebvr/vrdb/VRDB-SOURCE/VRDB/public/",
+    pathPrefix: "/home/respuit/Documents/Projects/mockupwebvr/vrdb/VRDB-SOURCE/VRDB/public/",
     breaks: true
-    
 }
